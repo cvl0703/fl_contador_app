fb8e49490ea658203edfe3f9af112391ed42df3f
cvl0703 <cvl0703@gmail.com>
2025-10-17 09:51:04 +0200

    primer commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(Object context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(child: const Text('homescreen nuevo')),
+    );
+  }
+}
