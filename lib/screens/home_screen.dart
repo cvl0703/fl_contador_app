ef72b94b6e0b9bb363fc76c2e542709baf1930b8
cvl0703 <cvl0703@gmail.com>
2025-10-17 10:15:40 +0200

    terminando por hoy

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
-  Widget build(Object context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey,
-      body: Center(child: const Text('homescreen nuevo')),
+      appBar: AppBar(title: const Text('Home Screen'), elevation: 10.0),
+      body: Center(
+        child: const Column(
+          children: <Widget>[
+            Text('Welcome to the Home Screen!'),
+            Text('Crear un homescreem'),
+            Expanded(child: FittedBox(child: FlutterLogo())),
+          ],
+        ),
+      ),
     );
   }
 }
